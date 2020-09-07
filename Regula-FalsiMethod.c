1459a65bbcf826bb57581ad5c48fc34dd6e5cb1c
Shubhankar-afk <shubhankar.30-cse-19@mietjammu.in>
2020-09-07 23:11:13 +0530

    converted double to float

--- Regula-FalsiMethod.c
+++ Regula-FalsiMethod.c
@@ -4,32 +4,34 @@
 #define tolerance 0.001		//Value of tolerance is defined
 
 //Function defination for the mathematical function of x
-double f(double x){
-	double y;
+float f(float x){
+	float y;
 	y=pow(x, 3)-5*x+3;
 	return y;
 }
 
 //Function for the calculation of x3
-double formula(double a, double b){
-	double y;
+float formula(float a, float b){
+	float y;
 	y=(a*f(b)-b*f(a))/(f(b)-f(a));
 	return y;
 }
 
 int main(){
 	int i=0, count=0;
-	double a, b, x3, oldX=0;
+	float a, b, x3, oldX;
 	printf("Enter the lower limit: ", a);
-	scanf("%lf", &a);
+	scanf("%f", &a);
 	printf("Enter the upper limit: ", b);
-	scanf("%lf", &b);
+	scanf("%f", &b);
 	do{
 		count++;
-		oldX=x3;		//Couldn't find a better way to do this.
 		x3=formula(a, b);
+		if(fabs(oldX-x3)<=tolerance){
+			i++;
+		}
 		if(f(x3)==0){
-			printf("%lf = the required root!\n", x3);
+			printf("%f = the required root!\n", x3);
 			break;
 		}
 		else if(f(a)*f(x3)>0){
@@ -38,7 +40,8 @@ int main(){
 		else{
 			b=x3;
 		}
-		printf("Root after iteration %d: %lf\n", count, x3);
-	}while(fabs(oldX-x3)>=tolerance);
-	printf("Final root after %d iterations: %.3lf\n", count, x3);
+		oldX=x3;
+		printf("Root after iteration %d: %f\n", count, x3);
+	}while(i<2);
+	printf("Final root after %d iterations: %.3f\n", count, x3);
 }
